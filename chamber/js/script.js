6b5b39ab04ca73c2ee0b9c304483fd304c807bde
eddyal20 <edisson.renteria.1993@gmail.com>
2022-06-05 00:34:07 -0600

    NEW

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,4 +1,8 @@
-// // HEADER:
+let date = new Date();
+let year = date.getFullYear();
+
+
+// // HEADER DATE:
 const todayDate = new Date();
 const day = todayDate.getDay()
 const month = todayDate.getMonth()
@@ -9,14 +13,33 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 const monthNames = [ "January", "February", "March", "April", "May", "June",
 "July", "August", "September", "October", "November", "December" ];
 
+// MONDAY & TUESDAY BANNER ANNOUNCEMENT:
+const parent = document.querySelector('#msg-box');
+
+
+if (date.getDay() == 1 || date.getDay() == 2) {
+
+    parent.style.display = 'show';
+}
+
+// HAMBURGER MENU:
+const hamburgerBtn = document.querySelector('.ham');
+const navigation = document.querySelector('.navigation')
+
+function toggleMenu() {
+    navigation.classList.toggle('responsive');
+    hamburgerBtn.classList.toggle('responsive');
+}
+
+hamburgerBtn.addEventListener('click', toggleMenu, false);
+
 // FOOTER:
 let currentdate = document.lastModified;
 document.querySelector('#lastdate').textContent = currentdate;
 console.log(currentdate);
 
 // DYNAMIC YEAR AT FOOTER
-let date = new Date();
-let year = date.getFullYear();
+
 
 document.querySelector('#year').textContent = year;
 
