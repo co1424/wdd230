40f2f09e2c60675e3cf05b662eaccc88f08921bf
eddyal20 <edisson.renteria.1993@gmail.com>
2022-06-16 12:28:08 -0600

    new

--- lesson-9/prophets.js
+++ lesson-9/prophets.js
@@ -8,7 +8,7 @@ fetch(requestURL)
     .then(function (jsonObject) {
             console.log(jsonObject); // temporary checking for valid response and data parsing.
             const prophets = jsonObject['prophets'];
-            prophets.forEach(displayTable);
+            prophets.forEach(displayProphets);
         }
 
     );
